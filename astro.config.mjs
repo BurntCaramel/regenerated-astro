c2bc8e87f1611c8cc0ad673c3ddf8cf08349dc6a
Patrick Smith <pgwsmith@gmail.com>
2022-08-15 13:23:33 +1000

    Add Deno build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
-
 import mdx from "@astrojs/mdx";
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: deno(),
 	integrations: [
 		// Enable Preact to support Preact JSX components.
 		preact(),
